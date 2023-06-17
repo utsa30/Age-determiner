bdce2555020cd7f4f5a9f06d362f06786775e738
utsa30 <82923739+utsa30@users.noreply.github.com>
2023-06-17 18:47:02 +0530

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,7 @@
+print ("Hello_Mr.")
+age1 = input ("What's Your Age?")
+age2 = input ("What Is Your Friend's Age?")
+if age1 > age2 :
+  print ("Your Are Elder Than Your Friend.")
+else:
+  print ("Your Are Elder Than Your Friend.")
